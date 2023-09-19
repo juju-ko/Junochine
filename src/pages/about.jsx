725c2cf95788a5ee8424069c17d33450b8e15ee7
juno-koo <juno-koo@pm.me>
2023-09-19 10:00:46 +0300

    Changed logo movement to all pages.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Helmet } from "react-helmet";
 
 import NavBar from "../components/common/navBar";
@@ -12,11 +12,49 @@ import SEO from "../data/seo";
 import "./styles/about.css";
 
 const About = () => {
+	const [stayLogo, setStayLogo] = useState(false);
+	const [logoSize, setLogoSize] = useState(80);
+	const [oldLogoSize, setOldLogoSize] = useState(80);
+
 	useEffect(() => {
 		window.scrollTo(0, 0);
 	}, []);
 
+	useEffect(() => {
+		const handleScroll = () => {
+			let scroll = Math.round(window.pageYOffset, 2);
+
+			let newLogoSize = 80 - (scroll * 4) / 10;
+
+			if (newLogoSize < oldLogoSize) {
+				if (newLogoSize > 40) {
+					setLogoSize(newLogoSize);
+					setOldLogoSize(newLogoSize);
+					setStayLogo(false);
+				} else {
+					setStayLogo(true);
+				}
+			} else {
+				setLogoSize(newLogoSize);
+				setStayLogo(false);
+			}
+		};
+
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, [logoSize, oldLogoSize]);
+
 	const currentSEO = SEO.find((item) => item.page === "about");
+
+	const logoStyle = {
+		display: "flex",
+		position: stayLogo ? "fixed" : "relative",
+		top: stayLogo ? "3vh" : "auto",
+		zIndex: 999,
+		border: stayLogo ? "1px solid white" : "none",
+		borderRadius: stayLogo ? "50%" : "none",
+		boxShadow: stayLogo ? "0px 4px 10px rgba(0, 0, 0, 0.25)" : "none",
+	};
 
 	return (
 		<React.Fragment>
@@ -32,9 +70,9 @@ const About = () => {
 			<div className="page-content">
 				<NavBar active="about" />
 				<div className="content-wrapper">
-					<div className="about-logo-container">
-						<div className="about-logo">
-							<Logo width={46} />
+					<div className="homepage-logo-container">
+						<div style={logoStyle}>
+							<Logo width={logoSize} link={false} />
 						</div>
 					</div>
 
