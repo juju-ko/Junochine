6bac9b25dff4b98a4e5094e2cb831c65edc937ca
juno-koo <juno-koo@pm.me>
2023-09-22 12:48:36 +0300

    Contact Form tests without proper css.

--- src/components/contact/contactForm.jsx
+++ src/components/contact/contactForm.jsx
@@ -1,22 +1,50 @@
-import React from "react";
+import { React, useState } from "react";
 
 import "./styles/contactForm.css";
 
 function ContactForm() {
-        return(
-            <div className="wrapper">
-              <h1>How About Them Apples</h1>
-              <form>
-              <fieldset>
-                 <label>
-                   <p>Name</p>
-                   <input name="name" />
-                 </label>
-               </fieldset>
-               <button type="submit">Submit</button>
-              </form>
-            </div>
-        )
-    }
+  const [inputs, setInputs] = useState({});
+
+  const [textarea, setTextarea] = useState(
+    "The content of a textarea goes in the value attribute"
+  );
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+    setTextarea(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(inputs);
+  }
+
+  return(
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <label>Enter your name:
+        <input 
+          type="text" 
+          name="username" 
+          value={inputs.username || ""} 
+          onChange={handleChange}
+        />
+        </label>
+        <label>Enter your age:
+          <input 
+            type="number" 
+            name="age" 
+            value={inputs.age || ""} 
+            onChange={handleChange}
+          />
+        </label>
+        <textarea value={textarea} onChange={handleChange} />
+        <input type="submit" />
+      </form>
+    </div>
+  )
+}
 
 export default ContactForm;
